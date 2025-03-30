d7f7b23d29a67a1be3d9f17cfacacc921ca6000e
Tetiana Dotsenko <doctatyana1@gmail.com>
2025-03-30 21:57:50 +0200

    add create article

--- page-objects/homePageSignedIn.ts
+++ page-objects/homePageSignedIn.ts
@@ -7,11 +7,30 @@ export class HomePageSignedIn{
         this.page=page
     }
     
+
+    async createArticle(title: string, description: string, body: string){
+        await this.page.getByText('New Article').click()
+        await this.page.locator('[formcontrolname="title"]').fill(title);
+        await this.page.locator('[formcontrolname="description"]').fill(description);
+        await this.page.locator('[formcontrolname="body"]').fill(body);
+        await this.page.getByRole('button', {name: "Publish Article"}).click()
+        await this.page.waitForURL('https://conduit.bondaracademy.com/article/**')
+    }
+
     async deleteArticle(){
-    await this.page.getByRole('button', {name: "Delete Article"}).first().click()
+        await this.page.getByRole('button', {name: "Delete Article"}).first().click()
     }
 
     async verifyArticleListTitleDoesNotContain(title: string) {
         await expect(this.page.locator('app-article-list h1').first()).not.toContainText(title);
+    }
+
+    getArticleTitleCreated(): Locator {
+        return this.page.locator('.banner h1')
+    }
+
+    async verifyArticleCreatedHasTitle(title: string) {
+        await this.page.waitForSelector('.banner h1')
+        await expect(this.getArticleTitleCreated()).toHaveText(title);
     }
 }
